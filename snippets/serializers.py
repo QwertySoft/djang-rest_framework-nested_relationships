2b78beec3797a53ae77f5de500f92e7ef194e275
Máximo Zarza <zmaximo1990@gmail.com>
2018-11-19 13:18:10 -0300

    Finish example

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -15,10 +15,15 @@ class PublisherSerializer(serializers.ModelSerializer):
 # Definimos un model serializer para el Snippet
 class SnippetSerializer(serializers.ModelSerializer):
     created = serializers.DateTimeField(read_only=True)
+    # Definimos el serializador a usar para el Publisher de un Snippet
+    publisher = PublisherSerializer(read_only=True)
+    # Utilizamos un campo para actualizar la referencia (clave foránea) de un Snippet a un Publisher
+    publisher_id = serializers.IntegerField(required=False)
 
     # Epecificamos meta-datos
     class Meta:
         # Estamos definiciendo un serializador para el modelo Snippet
         model = Snippet
         # Idicamos que campos se van a tener en cuenta para la des/serializacion
-        fields = ('id', 'title', 'code', 'language', 'created', 'publisher')
+        fields = ('id', 'title', 'code', 'language', 'created', 'publisher', 'publisher_id')
+
